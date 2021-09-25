268c22a05b4bdbbe8253e6840517f0dc81c6ab2e
aekryz1993 <ryzaek1993@gmail.com>
2021-09-25 10:38:00 +0000

    fix docker images

--- server/config/redis.js
+++ server/config/redis.js
@@ -2,13 +2,10 @@ import { createClient } from "redis";
 
 export default function redisConnect() {
   const client = createClient({
+    password: config.password,
     socket: {
-      url: `redis://${config.username}:${config.password}@${config.host}:${config.port}`,
-      // host: config.host,
-      // port: config.port,
-      // username: config.username,
-      // password: config.password,
-      // tls: true,
+      host: config.host,
+      port: config.port,
     },
   });
   (async () => {
@@ -23,8 +20,7 @@ export default function redisConnect() {
 }
 
 const config = {
-  host: process.env.REDIS_HOST || "127.0.0.1",
+  host: process.env.REDIS_HOST || "redis",
   port: process.env.REDIS_PORT || 6379,
-  username: process.env.REDIS_USER || "default",
-  password: process.env.REDIS_PASSWORD || 123456,
+  password: process.env.REDIS_PASSWORD,
 };
