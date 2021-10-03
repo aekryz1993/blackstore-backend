06be342376c68c8dc1f03a67cab82e655bec672c
aekryz1993 <ryzaek1993@gmail.com>
2021-10-03 15:57:02 +0000

    clean

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,29 +1,65 @@
-import app, { redisClient } from "./app";
-import { hostServer } from "./config/server.config";
-import { createAdmin } from "./db/seed";
-import sequelize from "./config/db.config";
-import http from "http";
+import express from "express";
+import session from "cookie-session";
 
-const config = {
-  host: hostServer(app)["host"],
-  port: hostServer(app)["port"],
+import {
+  localPassportStrategy,
+  SESSION_SECRET_VALUE,
+  expirySessionDate,
+} from "../config/passport.config";
+import authRouter from "./auth";
+import userSessionRouter from "./userSession";
+import {
+  checkActivePermission,
+  checkAdminPermission,
+} from "../controllers/middleware/permissions";
+import adminSessionRouter from "./adminSession";
+import { webhookEvents } from "../controllers/payment";
+
+const router = express.Router();
+
+const wrap = (middleware) => (socket, next) =>
+  middleware(socket.request, {}, next);
+
+const apiRouter = (app, passport, io, redisClient) => {
+  const sessionMiddleware = session({
+    name: "session",
+    keys: [SESSION_SECRET_VALUE],
+    cookie: {
+      secure: true,
+      httpOnly: true,
+      expires: expirySessionDate,
+    },
+  });
+
+  app.use(sessionMiddleware);
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  io.use(wrap(sessionMiddleware));
+  io.use(wrap(passport.initialize()));
+  io.use(wrap(passport.session()));
+
+  localPassportStrategy(passport);
+
+  // ensureSocketAuthorized(io)
+
+  router.use("/auth", authRouter(passport));
+  router.use(
+    "/userSession",
+    passport.authenticationMiddleware,
+    checkActivePermission,
+    userSessionRouter(io, redisClient)
+  );
+  router.use(
+    "/adminSession",
+    passport.authenticationMiddleware,
+    checkActivePermission,
+    checkAdminPermission,
+    adminSessionRouter(io, redisClient)
+  );
+  router.post('/coinbase/webhook', webhookEvents);
+  return router;
 };
 
-(async () => {
-  try {
-    const server = http.createServer(app);
-    const io = app.io;
-    io.attach(server);
-    server.listen(config.port, () => {
-      console.log('Connection has been established successfully on port ', config.port);
-    });
-    server.on("error", (err) => {
-      throw err;
-    });
-    await sequelize.sync();
-    await sequelize.authenticate();
-    createAdmin(redisClient);
-  } catch (err) {
-    console.error(err);
-  }
-})();
+export default apiRouter;
+
