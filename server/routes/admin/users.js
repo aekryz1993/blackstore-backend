8100a7a156e2484232193f71d37aa1351a6c5500
aekryz1993 <ryzaek1993@gmail.com>
2021-10-12 12:42:40 +0100

    update add user api

--- server/routes/admin/users.js
+++ server/routes/admin/users.js
@@ -3,7 +3,7 @@ import express from 'express'
 // import { addPicture } from '../../controllers/image';
 // import uploadImage from '../../controllers/middleware/image';
 import uploadExcel, { readExcel } from '../../controllers/middleware/excel';
-import { addUser, getAllUsers, addMultiUser, updateProfilePicture, confirmPayment } from "../../controllers/user";
+import { addUser, getAllUsers, addMultiUser, confirmPayment } from "../../controllers/user";
 import { addWallet, updateCredit } from '../../controllers/wallet';
 import { addPayMethod, fetchNotConfirmedPayments } from '../../controllers/payment';
 import { checkPermission } from '../../controllers/middleware/permissions';
@@ -13,11 +13,10 @@ const router = express.Router();
 const usersRouter = (redisClient) => {
 
   router.post('/add', checkPermission('addUser'), addUser(redisClient));
-  // router.post('/add', checkPermission('addUser'), uploadImage.single('picture'), addUser(redisClient), addWallet, addPicture);
   router.post('/addMulti', checkPermission('addUser'), uploadExcel(false), readExcel, addMultiUser(redisClient));
+  router.get('/update/', checkPermission('viewUser'), getAllUsers())
   router.get('/getusers/:page', checkPermission('viewUser'), getAllUsers())
   router.put('/updateCredit/:userId', checkPermission('updateCredit'), updateCredit)
-  // router.put('/updateUserPicture/:userId', checkPermission('updateUser'), uploadImage.single('picture'), updateProfilePicture, addPicture)
   router.put('/confirmPayment/:id', checkPermission('confirmPayment'), confirmPayment)
   // router.get('/payments', checkPermission('confirmPayment'), fetchNotConfirmedPayments)
   // router.post('/addPayMethod', checkPermission('addPayMethod'), addPayMethod)
