37b694308ff92069a3dd4f61ef12855e339be532
aekryz1993 <ryzaek1993@gmail.com>
2021-10-14 13:07:38 +0100

    update wallet

--- server/controllers/wallet.js
+++ server/controllers/wallet.js
@@ -24,13 +24,13 @@ export const updateCredit = (req, res) => {
 			euro = euro ? euro : 0.00
 			dinnar = dinnar ? dinnar : 0.00
 			const UserId = req.params.userId
-			const wallet = await walletQueries.find(UserId)
-			if (!wallet) {
-				res.status(401).json({message: 'This wallet doesn\'t exist'})
-			}
-			dollar = wallet.dataValues.dollar + parseFloat(dollar)
-			euro = wallet.dataValues.euro + parseFloat(euro)
-			dinnar = wallet.dataValues.dinnar + parseFloat(dinnar)
+			// const wallet = await walletQueries.find(UserId)
+			// if (!wallet) {
+			// 	res.status(401).json({message: 'This wallet doesn\'t exist'})
+			// }
+			// dollar = wallet.dataValues.dollar + parseFloat(dollar)
+			// euro = wallet.dataValues.euro + parseFloat(euro)
+			// dinnar = wallet.dataValues.dinnar + parseFloat(dinnar)
             await walletQueries.updateAll({UserId, dollar, euro, dinnar})
             return res.status(200).json({message: 'تم تحديث المحفظة بنجاح', success: true})
 		} catch (err) {
