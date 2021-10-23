30a80359cc48f3f56ef8813a4c6a54edf0319ea8
aekryz1993 <ryzaek1993@gmail.com>
2021-10-23 17:44:40 +0100

    test server

--- server/app.js
+++ server/app.js
@@ -3,15 +3,14 @@ import logger from "morgan";
 import compression from "compression";
 import helmet from "helmet";
 import cors from "cors";
-import passport from "passport";
+// import passport from "passport";
 import path from "path";
 import { Server } from "socket.io";
-import http from 'http';
 //const { createAdapter } = require("@socket.io/cluster-adapter");
 //const { setupWorker } = require("@socket.io/sticky");
 
 import { SESSION_SECRET, SESSION_SECRET_VALUE } from "./config/passport.config";
-import apiRouter from "./routes";
+// import apiRouter from "./routes";
 import redisConnect from "./config/redis";
 
 const app = express();
@@ -33,7 +32,7 @@ app.use(
 app.use(express.static(path.join(CURRENT_WORKING_DIR, "certificate")));
 app.use(express.static(path.join(CURRENT_WORKING_DIR, "resources")));
 app.set(SESSION_SECRET, SESSION_SECRET_VALUE);
-export const httpServer = http.createServer(app);
+
 const io = new Server();
 app.io = io;
 
@@ -44,7 +43,7 @@ const io = new Server(httpServer);
 
 // setupWorker(io);
 
-app.use("/api", apiRouter(app, passport, io, redisClient));
+// app.use("/api", apiRouter(app, passport, io, redisClient));
 
 app.get("/", function (req, res) {
    res.send('Welcome to Black Store GB.')
--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
-import app, { redisClient, httpServer } from "./app";
-//import app, { redisClient } from "./app";
+// import app, { redisClient, httpServer } from "./app";
+import app, { redisClient } from "./app";
 import { hostServer } from "./config/server.config";
 import { createAdmin } from "./db/seed";
 import sequelize from "./config/db.config";
-//import http from "http";
+import http from "http";
+import passport from "passport";
+import apiRouter from "./routes";
 
 const config = {
   host: hostServer(app)["host"],
@@ -12,11 +14,17 @@ const config = {
 
 (async () => {
   try {
-    //const httpServer = http.createServer(app);
-    //const io = app.io;
-    //io.attach(httpServer);
+    const httpServer = http.createServer(app);
+    const io = app.io;
+    io.attach(httpServer);
+
+    app.use("/api", apiRouter(app, passport, io, redisClient));
+
     httpServer.listen(config.port, () => {
-      console.log('Connection has been established successfully on port ', config.port);
+      console.log(
+        "Connection has been established successfully on port ",
+        config.port
+      );
     });
     httpServer.on("error", (err) => {
       throw err;
