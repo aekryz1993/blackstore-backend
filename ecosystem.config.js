f7e64d8c47fe16cb5b658b729d7fb7c647f5796b
aekryz1993 <ryzaek1993@gmail.com>
2021-10-23 18:23:20 +0000

    cluster

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+/*module.exports = {
+    apps: [
+        {
+	    name: "blue-store-server",
+            script: "./dist/main.js",
+            merge_logs: true,
+            instances : "max",
+            exec_mode: "cluster",
+            env: {
+                NODE_ENV: "production"
+            }
+        }
+    ]
+}*/
+
+module.exports = {
+  apps: [
+    {
+      name: "bluestore",
+      script: "./dist/main.js",
+      merge_logs: true,
+      instances: "max",
+      exec_mode: "cluster",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
