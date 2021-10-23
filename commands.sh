fefa8b8d1644f1e0922adbd4fa7dff6fb355bd50
aekryz1993 <ryzaek1993@gmail.com>
2021-10-23 19:02:36 +0000

    test

--- commands.sh
+++ commands.sh
@@ -1,6 +1,6 @@
 docker-compose stop
-#docker-compose down --rmi all
-#docker rmi $(docker images -a -q) -f
-#docker images
+docker-compose down --rmi all --volumes
+docker rmi $(docker images -a -q) -f
+docker images
 docker-compose up --build -d
 docker-compose logs --follow
